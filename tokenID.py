12804a86f774a96c7a4c8d6ff09084bc7df10c38
miguel-mf <59932523+miguel-mf@users.noreply.github.com>
2021-04-19 14:09:13 -0400

    Create tokenID.py

---
+++ tokenID.py
@@ -0,0 +1,9 @@
+#https://api.telegram.org/bot1794211659:AAFmoPsRKtdKTPHf4lD6CC38YMD79zhxgLg/getUpdates
+
+# API token
+telegram_token_eew_santiago_bot="1794211659:AAFmoPsRKtdKTPHf4lD6CC38YMD79zhxgLg"
+
+# Listado IDs canales
+eew_channel_id = -1001256292523
+eew_channel_M5_id = -1001372361727
+test_channel_id = -1001433764020
